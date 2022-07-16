70da9c25ab102d0ec7986143cfe0bf6f30c18177
Andrey Evseev <a-evseev@yandex.ru>
2022-07-16 18:47:52 +0300

    Решение задания 1

--- Task2_1 copy.py
+++ Task2_1 copy.py
@@ -1,26 +1,21 @@
 # Задание 1. Напишите программу, которая принимает на вход вещественное число и 
 # показывает сумму его цифр.
 
-from re import S
-
-
 def sum_nubber(n: float) -> int:
     s=str(n) 
     s=s.replace('.', '')
     s=int(s)
-    print(s)
     sum = 0
     while s != 0:
         sum = sum + s%10
-        print(s)
-    print(sum)
-    return
+        s //= 10
+    return sum
 
 
 print('Введите вещественное число: ', end=' ')
 n = float(input())
-sum_nubber(n)
-#print(n)
+sum = sum_nubber(n)
+print(sum)
 
 
 
